d6e8fc7ba6676ab4361ff571decdfcccc4b4a683
Warrick Wills <warrick.j@live.com>
2016-11-28 18:29:18 +1300

    Add files via upload

---
+++ DMS Assignment 1/src/DisconnectMessage.java
@@ -0,0 +1,13 @@
+
+/**
+ *
+ * @author Warrick Wills 13831575
+ * @author Keenen Leyson 13828049
+ */
+public class DisconnectMessage extends Message {
+
+    public DisconnectMessage() {
+        super("disconnected");
+    }
+
+}
